c45544e052c598cc5b0b5802f2421b894d309fb9
Luis Ibanez <luis.ibanez@kitware.com>
2014-05-24 17:52:39 -0400

    Now downloading data directly from StatCounter.
    Getting data straight from the source.

--- scripts/browser-www-monthly.py
+++ scripts/browser-www-monthly.py
@@ -9,7 +9,11 @@ import matplotlib.pyplot as plt
 
 # <codecell>
 
-browsers = pd.read_csv('browser-ww-monthly-200812-201402.csv')
+# Read data from the local disk
+# browsers = pd.read_csv('../data/browser-ww-monthly-200812-201402.csv')
+
+# Or download it from StatCounter directly
+browsers = pd.read_csv('http://gs.statcounter.com/chart.php?201402=undefined&device=Desktop%20%26%20Mobile%20%26%20Tablet%20%26%20Console&device_hidden=desktop%2Bmobile%2Btablet%2Bconsole&statType_hidden=browser&region_hidden=ww&granularity=monthly&statType=Browser&region=Worldwide&fromInt=200812&toInt=201402&fromMonthYear=2008-12&toMonthYear=2014-02&multi-device=true&csv=1')
 
 # <codecell>
 
